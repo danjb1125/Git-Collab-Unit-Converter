3c2eb0dd2c7d388587679090c905099dd6238f77
danjb1125 <danjhenielbringas1125@gmail.com>
2025-03-30 23:43:37 +0800

    Created the main function of the converter program

---
+++ Converter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Converter {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a meter length to convert into kilometers: ");
+        int meterLength = input.nextInt();
+        
+        input.close();
+    }
+}
